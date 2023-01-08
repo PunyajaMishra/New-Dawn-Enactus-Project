4ebb04991829122abf6ef7f9ea6cb64f41e508a9
Punyaja Mishra <punyajamishra@trentu.ca>
2023-01-07 21:59:44 -0500

    update hoempage

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,18 @@
+/*
+window.onscroll = function () {
+    myFunction()
+}
+
+var header = document.getElementById("header")
+var sticky = header.offsetTop
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky")
+    } else {
+        header.classList.remove("sticky")
+    }
+}*/
 // SCROLL ANIMATION //
 const scrollElements = document.querySelectorAll(".js-scroll")
 
